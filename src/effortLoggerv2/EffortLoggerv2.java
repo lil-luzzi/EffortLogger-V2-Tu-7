bae3f01549c3c408208dc674362f09c9123e59f3
pouiee <jonathan.jasso5@gmail.com>
2023-10-24 11:00:13 -0700

    Added Initial Project Files and .gitignore

---
+++ src/effortLoggerv2/EffortLoggerv2.java
@@ -0,0 +1,7 @@
+package effortLoggerv2;
+
+public class EffortLoggerv2 {
+	public static void main(String[] args) {
+		System.out.println("Hello Robert Lynn Carter!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EffortLoggerv2 {
+}
