e64cae68f7985d920e0a372a7dc7dff6fbbe8a54
Luz <lerod0907@gmail.com>
2023-10-25 02:29:46 -0700

    javafx application open testing

--- src/effortLoggerv2/EffortLoggerv2.java
+++ src/effortLoggerv2/EffortLoggerv2.java
@@ -1,7 +1,39 @@
 package effortLoggerv2;
 
-public class EffortLoggerv2 {
+
+/*public class EffortLoggerv2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Robert Lynn Carter!");
 	}
+}*/
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class EffortLoggerv2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("Luz Rodriguez Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Luz Rodriguez Hello World");
+        Button btn = new Button();
+        btn.setText("Display: 'bruh says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Luz Rodriguez: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,16 @@
  * 
  */
 module EffortLoggerv2 {
+	/*requires javafx.controls;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;*/
+	requires javafx.controls;
+	
+	opens effortLoggerv2 to javafx.graphics, javafx.fxml;
+
 }
