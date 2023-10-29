7b5e1daf5c6dbcbbe26267be917c89e7e0e3f587
lil-luzzi <luzzipoo@gmail.com>
2023-10-29 04:13:40 -0700

    login password hasher protoype v1

--- src/effortLoggerv2/LoginController.java
+++ src/effortLoggerv2/LoginController.java
@@ -18,30 +18,65 @@ public class LoginController {
 	@FXML
 	private TextField nameTextField;
 	
+	@FXML
+	private TextField passTextField;
+	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	private Boolean verification = false;
+	private Boolean userExists = false;
+	private String salt = LoginHasher.generateSalt();
+	private int userIndex;
+	LoginHasher pass = new LoginHasher();
+    private String[] hashedPass = {LoginHasher.hashPassword(pass.passwords[0], salt),LoginHasher.hashPassword(pass.passwords[1], salt),
+    		LoginHasher.hashPassword(pass.passwords[2], salt)};
 	
 	@FXML
 	public void login(ActionEvent event) throws IOException {
 		
-		
-		String username = nameTextField.getText();
-		System.out.println(username);
-		
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
-		root = loader.load();
-		
-		Controller mainController = loader.getController();
+		for (int i=0;i<3;i++) {
+			if(nameTextField.getText().equals(pass.usernames[i])) {
+				userIndex = i;
+				userExists = true;
+			}
+		}
 		
 		
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		stage.setScene(scene);
-		stage.show();
-	
+		if(userExists == true) {
+			String username = nameTextField.getText();
+			String password = passTextField.getText();
+			
+			verification = LoginHasher.verifyPassword(password, salt, hashedPass[userIndex]);
+
+			if(verification == true) {
+				System.out.println("Logging in...");
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+				root = loader.load();
+				
+				Controller mainController = loader.getController();
+				
+				
+				stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				stage.setScene(scene);
+				stage.show();
+			
+			}
+			else {
+				System.out.println("Wrong Password!");
+			}
+		}
+		else {
+			System.out.println("User does not exist.");
+		}
+		
+		
 	}
+		
+		
+		
 	
 }
 	
